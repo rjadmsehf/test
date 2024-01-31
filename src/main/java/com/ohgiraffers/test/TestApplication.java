d45016d82e4b6b84ec3c8e5fbb225dd85cb27601
pyunghun <jjg524@naver.com>
2024-01-31 14:30:51 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+
+    // 목표 오늘의 수업 목표입니다.
+
+    // 여기는 주석입니다.
+    /*여기는
+    * 여러 줄
+    * 주석 입니다
+    * */
+
+    /*ctrl - spase : 자동완성 기능
+    *ctrl - d 복사*/
+    // 복사예시
+    // 복사예시
+    // 복사예시
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다");
+    }
+
+}
